a5e2ae930adfb7359f03388f34407b972e0e0a06
sahilpeje <44080317+sahilpeje@users.noreply.github.com>
2021-03-15 20:02:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-model = pickle.load(open('G:\\HeartDisease\\knn_model.pkl', 'rb'))
+model = pickle.load(open('knn_model.pkl', 'rb'))
 
 
 @app.route('/',methods=['GET'])
